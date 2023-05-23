bb51d8095c2131401aa033c8fb2394863c073701
Nicolas Louca <nikolaslouca@gmail.com>
2023-05-23 10:47:15 +0300

    update

--- display.js
+++ display.js
@@ -1,8 +1,29 @@
 define(function (edit) {
   console.log("helo!");
 
+var loggin_user = "";
+
+pnp.sp.profiles.MyPrperties.get().then(function(result) {
+
+
+  console.log(result);
+  var props = result.UserProfileProperties;
+  for (var i = 0; i < props.length; i++) {
+    switch (props[i].Key) {
+      case 'username':
+        console.log(props[i].Value);
+        loggin_user = props[i].Value;
+        break;
+
+    }
+  }
   var dt = $(fd.control("SPDataTable1").$el);
   //hides 'Add new record' button
+  fd.control("SPDataTable1").widget.dataSource.filter({
+    field: fd.field("TECHNICIAN").value.EntityData.Email,
+    operator: "in",
+    value: loggin_user
+  });
   dt.find(".fd-datatable div.k-header.k-grid-toolbar").css("display", "none");
   //hides delete column
   dt.find(
