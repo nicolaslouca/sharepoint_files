552c4e415f933ecfba060bfb07d839bd9652e68b
Nicolas Louca <nikolaslouca@gmail.com>
2023-05-23 10:34:23 +0300

    update

--- display.js
+++ display.js
@@ -1,17 +1,11 @@
-define(function (edit){
-console.log("helo!");
+define(function (edit) {
+  console.log("helo!");
 
-
-  var dt = $(fd.control('SPDataTable1').$el);
-//hides 'Add new record' button  
-dt.find('.fd-datatable div.k-header.k-grid-toolbar').css('display', 'none')
-//hides delete column
-dt.find('.fd-datatable table tr th:last-child,.fd-datatable table tr td:last-child').css('display', 'none')
-})
-
-
+  var dt = $(fd.control("SPDataTable1").$el);
+  //hides 'Add new record' button
+  dt.find(".fd-datatable div.k-header.k-grid-toolbar").css("display", "none");
+  //hides delete column
+  dt.find(
+    ".fd-datatable table tr th:last-child,.fd-datatable table tr td:last-child"
+  ).css("display", "none");
 });
-
-
-
-})
